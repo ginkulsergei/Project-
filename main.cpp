885cff5eab03317b632479b5acb4383f84f457e7
ginkulsergei <48618761+ginkulsergei@users.noreply.github.com>
2019-05-19 20:38:57 +0200

    CAse 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
-//CASE 3 -Fixed interest rate and instalments, 1. With n,PV,r
+//
+//  main.cpp
+//  Project Case 1
+//
+//  Created by Serhii Hinkul on 20/04/2019.
+//  Copyright © 2019 Serhii Hinkul. All rights reserved.
+//
+
 #include <iostream>
 #include <fstream>
 #include <ctime>
@@ -7,12 +14,11 @@
 #include <limits>
 #include <string>
 #include <cmath>
-#include<iomanip>
 using namespace std;
 int main()
 {
-    double Pi, p, R, r, Rate;
-    int FQ, month, year, i, N;
+    double Pi, p, R, r;
+    int FQ, month, year, i, N, Rate;
     
     cout<< "Provide Number of periods(only integer) "<<endl;//check for input errors do it later
     cin>>N;
@@ -20,8 +26,7 @@ int main()
     double Openbalance[N];
     double Interest[N];
     double Closingbalance[N];
-    double PMTi;
-   
+    double PMTi[N];
 
     cout<< "Provide Present value"<<endl;
     cin>>p;
@@ -37,7 +42,7 @@ int main()
      }*/
     cout<< "Provide Intrest Rate (in percentage)"<<endl;// make a boundary check fr errors
     cin>>R;
-    cout<< "Provide Frequency (quarterly = 1 /n, semiannualy = 2 /n, annualy = 3)"<< "FQ"<<endl; //check for input errors do it later
+    cout<< "Provide Frequency (quarterly = 1 /n, semiannualy = 2 /n, annualy = 3)"<< "FQ"<<endl; //check for input errors do it
     cin>>FQ;
     if(FQ==1)
     {r=R/(4*100);}
@@ -45,74 +50,71 @@ int main()
     {r=R/(2*100);}
     if(FQ==3)
     {r=R/100;}
-    
 
     //cout<< "Provide Starting month and year in the format MM/YYYY"<<endl;//check for input errors do it later
     //cin>>month>>year;
     
     
+
     
-    
+    Pi=p/N; //principal
     
     // interest
     //instalment
+
     
-    
-    //assigning the value of yearly rate
+     //assigning the value of yearly rate
     //N=n; assigning the number of periods according to the frequency of payments
-    
+   
     Openbalance[0]=p;
-    
+
     
     char listSeparator=';';
     ofstream csvFile;
     ofstream outputFile;
-    csvFile.open ("Payments2.csv");
+    csvFile.open ("Payments1.csv");
     csvFile  << "Period #"<< listSeparator <<"Opening Balance"<< listSeparator\
     <<"Principal"<< listSeparator <<"Periodic Interest rate"<< listSeparator <<\
     "Interest"<< listSeparator <<"Instalment"<< listSeparator <<"Closing Balance\n";
     
-    PMTi=p*r*(1+1/(pow(1+r,N)-1));
-    cout << fixed << setprecision(2) << PMTi <<endl;
+    
     
     for(int i=0; i<=N-1;i++) //computing the PV, PMT, Pi for every period by using loop
-        //for(PViclose; PViclose>=0; PViclose=PViclose-Pi)
+    //for(PViclose; PViclose>=0; PViclose=PViclose-Pi)
     {
-        // PMTi=PVi1*r*(1+(1/((pow(1+r,N)-1))));
-        
-        //Pi[i]=PVi1-Pi;
+       // PMTi=PVi1*r*(1+(1/((pow(1+r,N)-1))));
+
+            //Pi[i]=PVi1-Pi;
         Interest[i]=Openbalance[i]*r;
-        Pi=PMTi-Interest[i];
+        PMTi[i]=Pi+Interest[i];
         Closingbalance[i]=Openbalance[i]-Pi;
         Openbalance[i+1]=Closingbalance[i];
         
         Rate=r*100;
-
         
         csvFile << i+1 << listSeparator  << Openbalance[i] << listSeparator << Pi\
         << listSeparator << Rate<<"%" << listSeparator << Interest[i] << listSeparator <<\
-        PMTi << listSeparator << Closingbalance[i] << "\n";
+        PMTi[i] << listSeparator << Closingbalance[i] << "\n";
         
-                cout<<Openbalance[i]<<endl;
-                cout<<Pi<<endl;
-                cout<<Rate<<"%"<<endl;
-                cout<<Interest[i]<<endl;
-                cout<<PMTi<<endl;
-                cout<<Closingbalance[i]<<endl;
-                cout << "====" << i << endl;
-        
-        
-        
+        cout<<Openbalance[i]<<endl;
+        cout<<Pi<<endl;
+        cout<<Rate<<"%"<<endl;
+        cout<<Interest[i]<<endl;
+        cout<<PMTi[i]<<endl;
+        cout<<Closingbalance[i]<<endl;
+        cout << "====" << i << endl;
+
+
+
     }
     csvFile.close();
     
     cout << "Files saved!" ;
     //cin<<"Provide PVi1";
     //PMTi=Pi+Inti;
-    // Inti=PVi1*R;
+   // Inti=PVi1*R;
     
-    
-    //print the results in Arrays,
-    
-}
 
+//print the results in Arrays,
+        
+}
